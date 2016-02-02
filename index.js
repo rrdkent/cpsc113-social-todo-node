69b4102d267680887ba0474908281c474546e039
rrdkent <rrdkent@gmail.com>
2016-02-02 00:15:12 +0000

    Parse submitted forms per Kyle's Video

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ var exphbs = require('express-handlebars');
 var app = express();
 
 
+//CONFIG STUFF
+
 //This was added from the https://github.com/ericf/express-handlebars under "Basic Usage"
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+// This is for the body parser.  Not sure what it does, but Kyle says we need it.
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
 
 app.get('/', function (req, res) {
     res.render('index');
+});
+
+
+//Here we create a controller to submit the code to the DB (2:09 from social todo handle submitted registration form video)
+app.post('/user/register', function (req, res) {
+    res.send(req.body);
+    console.log('The User has the E-mail address', req.body.email);
 });
 
 
